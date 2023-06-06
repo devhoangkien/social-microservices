122ee47ed90fe3c83b7d7ae8fe5de65f521838f8
devhoangkien <devhoangkien@gmail.com>
2023-06-06 08:47:04 +0700

    add docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd gateway && npm run copy:protos && npm run build && cd -
-cd apps/auth && npm run copy:protos && npm run build && cd -
+cd gateway && rm -rf dist && npm run build && cd -
+cd apps/auth  && rm -rf dist && npm run build && cd -
 
 docker-compose build --no-cache
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd gateway && npm i && cd -
-cd apps/auth && npm i && cd -
+cd gateway && rm -rf dist && rm -rf node_modules && npm i && cd -
+cd apps/auth &&  rm -rf dist && rm -rf node_modules && npm i && cd -
